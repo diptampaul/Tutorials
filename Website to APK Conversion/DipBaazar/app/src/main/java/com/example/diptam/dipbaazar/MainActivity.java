999aed603d0a46477e5538f0826e38ba66b9a014
Diptam <diptampaul13@gmail.com>
2020-01-16 20:05:45 +0530

    Website to APK Added

---
+++ Website to APK Conversion/DipBaazar/app/src/main/java/com/example/diptam/dipbaazar/MainActivity.java
@@ -0,0 +1,50 @@
+package com.example.diptam.dipbaazar;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    private WebView mWebView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mWebView = (WebView) findViewById(R.id.activity_main_webview);
+        WebSettings webSettings = mWebView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        mWebView.loadUrl("https://www.shop101.com/dipbaazar");
+        mWebView.setWebViewClient(new com.example.diptam.dipbaazar.MyAppWebViewClient(){
+            @Override
+            public void onPageFinished(WebView view, String url) {
+                //hide loading page
+                findViewById(R.id.splashloading01).setVisibility(View.GONE);
+                //show Webview
+                findViewById(R.id.activity_main_webview).setVisibility((View.VISIBLE));
+            }
+        }
+        );
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (mWebView.canGoBack()) {
+            mWebView.goBack();
+        }
+        else {
+            super.onBackPressed();
+        }
+    }
+
+
+    private class MyAppWebViewClient extends WebViewClient{
+
+    }
+}
---
+++ Website to APK Conversion/DipBaazar/app/src/main/java/com/example/diptam/dipbaazar/MyAppWebViewClient.java
@@ -0,0 +1,19 @@
+package com.example.diptam.dipbaazar;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MyAppWebViewClient extends WebViewClient {
+    @Override
+    public boolean shouldOverrideUrlLoading(WebView view, String url) {
+        if(Uri.parse(url).getHost().endsWith("shop101.com/dipbaazar")) {
+            return false;
+        }
+
+        Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
+        view.getContext().startActivity(intent);
+        return true;
+    }
+}
